399b7206f5f95f8466a35da47308aa415f930607
Mikuso <g_github@flamescape.com>
2013-11-09 12:51:24 +0000

    More map parsing

--- shared/Maze.js
+++ shared/Maze.js
@@ -27,12 +27,51 @@ Maze.prototype.load = function(levelNum, cb) {
 };
 
 Maze.prototype.parse = function(data) {
-    data = data.replace(/\s/gm, '').split('');
+    // remove any nasty carriage returns
+    data = data.replace(/\r/g, '');
+    
+    // determine the map width by the first newline
+    this.width = data.indexOf("\n");
+    
+    // get the map data as an array of tiles
+    data = data.replace(/\n/g, '').split('');
+    
+    // determine the map height by the number of rows
+    this.height = (data.length / this.width) << 0;
+    
+    // create collision map
     this.collisions = _.map(data, function(tile){
         return tile === '#' ? 1 : 0;
     });
+    
+    // create pill map
     this.pills = _.map(data, function(tile){
         return tile === '.' ? 1 : (tile === '@' ? 2 : 0);
     });
-    //console.log(this);
+    
+    var tileAt = function(idx) {
+        if (idx < 0 || idx >= this.collisions.length)
+            return 1;
+        
+        return this.collisions[idx];
+    }.bind(this);
+    
+    // create wall decoration map
+    this.wallDecor = _.map(this.collisions, function(tile, idx){
+        if (!tile)
+            return 0;
+        
+        return (tileAt(idx+1) ? 1 : 0)
+             | (tileAt(idx+1+this.width) ? 2 : 0)
+             | (tileAt(idx+this.width) ? 4 : 0)
+             | (tileAt(idx+this.width-1) ? 8 : 0)
+             | (tileAt(idx-1) ? 16 : 0)
+             | (tileAt(idx-this.width-1) ? 32 : 0)
+             | (tileAt(idx-this.width) ? 64 : 0)
+             | (tileAt(idx+1-this.width) ? 128 : 0)
+             ;
+    });
+    
+    //console.log(this.decor);
+    
 };
