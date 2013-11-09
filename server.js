bc008187b32698043a9a37b071679b9ca5c55716
Mikuso <g_github@flamescape.com>
2013-11-09 00:13:12 +0000

    Fix for Windows

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ http.createServer(function (req, res) {
   if (err) { console.error(err); process.exit(-1); }
 
   // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
+  if (process.getuid && process.getuid() === 0) {
     require('fs').stat(__filename, function(err, stats) {
       if (err) { return console.error(err); }
       process.setuid(stats.uid);
