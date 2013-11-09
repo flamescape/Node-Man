d1b53a5144df884a0d560d9d69fb16600cf247d2
John Ballantyne <johnballantyne@gmail.com>
2013-11-08 22:39:52 -0500

    Add some Kinetic groundwork

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,16 @@
+var _ = require('underscore');
+
 var m = new Maze();
 m.load(1, function(err){
     if (err) throw err;
     
     // loaded maze into m
 });
+
+var stage = new Kinetic.stage({
+    container: 'map',
+    width: 560,
+    height: 620
+});
+
+var mapLayer = new Kinetic.layer();
