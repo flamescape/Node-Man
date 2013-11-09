08113868101a0b60157c2cd4480bc07ddd3abf6b
Mikuso <g_github@flamescape.com>
2013-11-09 17:14:06 +0000

    More classes

--- shared/Character.js
+++ shared/Character.js
@@ -3,8 +3,42 @@ var EventEmitter2 = EventEmitter2 || require('eventemitter2');
 var Character = function() {
     this.x = 0;
     this.y = 0;
+    /* up:0, right:1, down:2, left:3 */
+    this.direction = 0;
+    this.speed = 1;
 };
 Character.prototype.__proto__ = EventEmitter2.prototype;
+ 
+Character.prototype.tick = function() {
+    var mx = 0, my = 0;
+    
+    switch (this.direction) {
+        case 0: my -= 1; break;
+        case 1: mx += 1; break;
+        case 2: my += 1; break;
+        case 3: mx -= 1; break;
+    }
+    
+    // check for wall in front of us
+    // if wall - mx = 0, my = 0
+    
+    this.x += mx;
+    this.y += my;
+};
 
-Character.prototype.a = function() {
+Character.prototype.changeDirection = function(newDir) {
+    switch (newDir) {
+        case 0:
+        case 2:
+            if (this.x % 10 === 0) {
+                this.direction = newDir;
+            }
+            break;
+        case 1:
+        case 3:
+            if (this.y % 10 === 0) {
+                this.direction = newDir;
+            }
+            break;
+    }
 };
