fae812f58d5f96341f0d080e78c638f5aabed18b
John Ballantyne <johnballantyne@gmail.com>
2013-11-09 16:29:54 -0500

    Shadow for pills for depth

--- shared/Maze.js
+++ shared/Maze.js
@@ -139,7 +139,11 @@ Maze.prototype.createPillsLayer = function(tileSize, tiles) {
                 radius: 4,
                 sides: 6,
                 rotationDeg: Math.random() * (360 - 1) + 1,
-                fill: "#FFF"
+                fill: "#FFF",
+                shadowColor: "#666",
+                shadowBlur: 0,
+                shadowOffsetX: 2,
+                shadowOffsetY: 2 
             }));
         } else if (pill === 2) {
             superPillsLayer.add(new Kinetic.RegularPolygon({
@@ -149,7 +153,7 @@ Maze.prototype.createPillsLayer = function(tileSize, tiles) {
                 sides: 6,
                 rotationDeg: Math.random() * (360 - 1) + 1,
                 fill: "#FFF",
-                shadowColor: "888",
+                shadowColor: "#888",
                 shadowBlur: 1,
                 shadowOffsetX: 2,
                 shadowOffsetY: 2
