27a32b299700b2103f197321aca39e5617320672
Mikuso <g_github@flamescape.com>
2013-11-09 02:54:03 +0000

    Fix for CWD mis-alignment

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ global.SERVER = true;
 require('nko')('AMR6PQQjUxHNpPiC');
 require('colors');
 
-var port = process.env.NODE_ENV === 'production' ? 80 : 8000
+var path = require('path')
+  , port = process.env.NODE_ENV === 'production' ? 80 : 8000
   , express = require('express')
   , app = express()
   , server = require('http').createServer(app)
@@ -15,7 +16,7 @@ var port = process.env.NODE_ENV === 'production' ? 80 : 8000
 
 server.listen(port, function(err) {
     if (err) { console.error(err); process.exit(-1); }
-    console.log((new Date()).toString(), 'Listening on port', port);
+    console.log('Listening on port', port);
     // if run as root, downgrade to the owner of this file
     if (process.getuid && process.getuid() === 0) {
         require('fs').stat(__filename, function(err, stats) {
@@ -25,15 +26,10 @@ server.listen(port, function(err) {
     }
 });
 
-app.use(function(req, res, next){
-    console.log('Request rec\'d from', req.ip, req.url);
-    next();
-});
-
-app.use(express.static('client'));
-app.use('/js/shared', express.static('shared'));
-app.use('/js/lib/shared', express.static('node_modules'));
-app.use('/levels', express.static('levels'));
+app.use(express.static(path.join(__dirname, 'client')));
+app.use('/js/shared', express.static(path.join(__dirname, 'shared')));
+app.use('/js/lib/shared', express.static(path.join(__dirname, 'node_modules')));
+app.use('/levels', express.static(path.join(__dirname, 'levels')));
 
 io.sockets.on('connection', function(sock) {
     console.log('New client connected.'.cyan);
