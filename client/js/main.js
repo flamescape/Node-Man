396ce66b060d0e32f33d249355764653bcd493ad
John Ballantyne <johnballantyne@gmail.com>
2013-11-08 23:13:10 -0500

    Let's draw some rects

--- client/js/main.js
+++ client/js/main.js
@@ -1,16 +1,32 @@
-var _ = require('underscore');
-
 var m = new Maze();
 m.load(1, function(err){
     if (err) throw err;
     
     // loaded maze into m
+    drawMaze();
 });
 
-var stage = new Kinetic.stage({
-    container: 'map',
+var stage = new Kinetic.Stage({
+    container: 'maze',
     width: 560,
     height: 620
 });
 
-var mapLayer = new Kinetic.layer();
+var mazeLayer = new Kinetic.Layer();
+
+var drawMaze = function() {
+    _.each(m.collisions, function(tile, num) {
+        mazeLayer.add(new Kinetic.Rect({
+            x: 0,
+            y: 0,
+            width: 56,
+            height: 62,
+            fill: "#EE0000"
+        }));
+
+    }); 
+
+    stage.add(mazeLayer);
+};
+
+
